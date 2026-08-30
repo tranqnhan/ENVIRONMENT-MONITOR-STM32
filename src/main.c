523caf6326cfdc83fca2e5714193887db399a0aa
nhtran <nhtran@iastate.edu>
2026-08-29 23:58:31 -0500

    serial output success

--- src/main.c
+++ src/main.c
@@ -33,6 +33,20 @@ int _write(int file, char *ptr, int len)
 void MX_USART2_UART_Init(void)
 {
     __HAL_RCC_USART2_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    // PA2 = USART2_TX
+    // PA3 = USART2_RX
+    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 
     huart2.Instance = USART2;
     huart2.Init.BaudRate = 115200;
